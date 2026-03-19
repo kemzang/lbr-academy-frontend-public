676e9f75f1151c997f37f8c997e49c313d9694d6
kemzang <kemzangbryan@gmail.com>
2026-03-19 19:01:46 +0100

    modification de la variable dans next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000";
+const BACKEND_URL = process.env.BACKEND_URL || process.env.URL_BACKEND || "http://localhost:3000";
 
 const nextConfig: NextConfig = {
   async rewrites() {
